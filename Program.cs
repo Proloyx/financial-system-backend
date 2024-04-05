6b46683e668667e7c0bd2a9aebf11fca625c7724
Eloy <109438329+Proloyx@users.noreply.github.com>
2024-04-05 12:49:49 -0400

    New Cors

--- Program.cs
+++ Program.cs
@@ -5,22 +5,13 @@ using FinancialSystemBackend.Services;
 
 
 DotNetEnv.Env.Load();
-var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(name: MyAllowSpecificOrigins,
-                      policy  =>
-                      {
-                          policy.WithOrigins("http://example.com",
-                                              "http://www.contoso.com");
-                      });
-});
+builder.Services.AddCors();
 builder.Services.AddSingleton<IRequest,Request>();
 //builder.Services.AddSingleton<IUrlBuilder,UrlBuilder>();
 var app = builder.Build();
@@ -29,7 +20,7 @@ var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
-app.UseCors(MyAllowSpecificOrigins);
+app.UseCors();
 app.UseRouting();
 app.UseAuthorization();
 app.MapControllers();
