b45613ab151644bee58f26364260f6c5d5ef5897
Eloy <eloycruzmartin@gmail.com>
2024-06-26 14:47:07 +0200

    Grafana Finished and Ready

--- Program.cs
+++ Program.cs
@@ -11,103 +11,103 @@ using Serilog;
 
 Env.Load();
 
-try
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddCors();
+
+builder.Services.AddAuthentication("Bearer").AddJwtBearer(opt => {
+        var symmetrickey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Env.GetString("Key")));
+        var signingcredentials = new SigningCredentials(symmetrickey, SecurityAlgorithms.HmacSha256Signature);
+        opt.RequireHttpsMetadata = false;
+
+        opt.TokenValidationParameters = new TokenValidationParameters(){
+                ValidateAudience = false,
+                ValidateIssuer = false,
+                IssuerSigningKey = symmetrickey
+        };        
+});
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(Env.GetString("DbConnection")));
+builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
 {
-    Log.Information("Starting up the host");
-    var builder = WebApplication.CreateBuilder(args);
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "FSObserver API", Version = "v1" });
 
-    builder.Host.UseSerilog((ctx,cfg) => cfg
-            .WriteTo.PostgreSQL(Env.GetString("DbConnection"), "logs", needAutoCreateTable: true,schemaName: "fso")
-            .Enrich.WithProperty("Application", ctx.HostingEnvironment.ApplicationName)
-            .WriteTo.Console()
-            );
-
-    builder.Services.AddControllers();
-    // Add services to the container.
-    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-    builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddCors();
-    
-    builder.Services.AddAuthentication("Bearer").AddJwtBearer(opt => {
-            var symmetrickey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Env.GetString("Key")));
-            var signingcredentials = new SigningCredentials(symmetrickey, SecurityAlgorithms.HmacSha256Signature);
-            opt.RequireHttpsMetadata = false;
-    
-            opt.TokenValidationParameters = new TokenValidationParameters(){
-                    ValidateAudience = false,
-                    ValidateIssuer = false,
-                    IssuerSigningKey = symmetrickey
-            };        
-    });
-    
-    builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(Env.GetString("DbConnection")));
-    builder.Services.AddSwaggerGen();
-    builder.Services.AddSwaggerGen(c =>
+    // Configuración de seguridad para Swagger
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
-        c.SwaggerDoc("v1", new OpenApiInfo { Title = "FSObserver API", Version = "v1" });
-    
-        // Configuración de seguridad para Swagger
-        c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-        {
-            Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
-                          Enter 'Bearer' [space] and then your token in the text input below.
-                          \r\n\r\nExample: 'Bearer 12345abcdef'",
-            Name = "Authorization",
-            BearerFormat = "JWT",
-            In = ParameterLocation.Header,
-            Type = SecuritySchemeType.ApiKey,
-            Scheme = "Bearer"
-        });
-    
-        c.AddSecurityRequirement(new OpenApiSecurityRequirement()
-        {
-            {
-                new OpenApiSecurityScheme
-                {
-                    Reference = new OpenApiReference
-                    {
-                        Type = ReferenceType.SecurityScheme,
-                        Id = "Bearer"
-                    },
-                    Scheme = "oauth2",
-                    Name = "Bearer",
-                    In = ParameterLocation.Header,
-    
-                },
-                new List<string>()
-            }
-        });
+        Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+        Name = "Authorization",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
     });
-    
-    builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
-    builder.Services.AddSingleton<IRequest,Request>();
-    var app = builder.Build();
 
-    app.UseSerilogRequestLogging();
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header,
 
-    // Configure the HTTP request pipeline.
-    app.UseSwagger();
-    app.UseSwaggerUI();
-    
-    app.UseCors(builder =>
-            builder
-            .WithOrigins("*")
-            .AllowAnyMethod()
-            .AllowAnyHeader());;
+            },
+            new List<string>()
+        }
+    });
+});
 
-    app.UseRouting();
-    app.UseAuthentication();
-    app.UseAuthorization();
-    app.MapControllers();
-    app.Run();
-}
-catch (Exception e)
-{
-    Log.Fatal(e, "Host terminated unexpectedly");
-}
-finally
-{
-    Log.CloseAndFlush();
-}
+builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
+builder.Services.AddSingleton<IRequest,Request>();
+var app = builder.Build();
+app.UseSerilogRequestLogging();
+// Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseCors(builder =>
+        builder
+        .WithOrigins("*")
+        .AllowAnyMethod()
+        .AllowAnyHeader());;
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
+
+
+// try
+// {
+//     Log.Information("Starting up the host");
+//     var builder = WebApplication.CreateBuilder(args);
+
+//     builder.Host.UseSerilog((ctx,cfg) => cfg
+//             .WriteTo.PostgreSQL(Env.GetString("DbConnection"), "logs", needAutoCreateTable: true,schemaName: "fso")
+//             .Enrich.WithProperty("Application", ctx.HostingEnvironment.ApplicationName)
+//             .WriteTo.Console()
+//             );
+// }
+// catch (Exception e)
+// {
+//     Log.Fatal(e, "Host terminated unexpectedly");
+// }
+// finally
+// {
+//     Log.CloseAndFlush();
+// }
 
 
