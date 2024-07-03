78cfe4c4e21525910405d5853e4e307d9372fa2e
Eloy <eloycruzmartin@gmail.com>
2024-07-03 15:41:23 +0200

    Adding massive update and delete

---
+++ FinancialSystem/Models/Massiveupdt.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinancialSystem.Models
+{
+    public class Massiveupdt
+    {
+        public string oldpass {get; set;}
+        public string newpass {get; set;}
+    }
+}
